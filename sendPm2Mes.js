18d058d42bb9a56fed83007655bd401c2625f97c
Maxim <haisarov-2002@mail.ru>
2024-03-19 15:10:43 +0500

    aaaaa

--- sendPm2Mes.js
+++ sendPm2Mes.js
@@ -3,31 +3,31 @@ const pm2 = require('pm2')
 pm2.connect((err) => {
 
 
-    // pm2.sendDataToProcessId({
-    //     id: 15,
-    //     type: 'process:msg',
-    //     data: {
-    //         morecom: 100
-    //     },
-    //     topic: 'comUpdate'
-    // }, (err, res) => {
-    //     if (err) console.error(err);
-    //     // else console.log(res);
-    // });
-
-
-
     pm2.sendDataToProcessId({
-        id: 11,
+        id: 16,
         type: 'process:msg',
         data: {
-            smallCom: 50
+            morecom: 100
         },
-        topic: 'updateComis'
+        topic: 'comUpdate'
     }, (err, res) => {
         if (err) console.error(err);
         // else console.log(res);
     });
+
+
+
+    // pm2.sendDataToProcessId({
+    //     id: 11,
+    //     type: 'process:msg',
+    //     data: {
+    //         smallCom: 50
+    //     },
+    //     topic: 'updateComis'
+    // }, (err, res) => {
+    //     if (err) console.error(err);
+    //     // else console.log(res);
+    // });
 
 
 
