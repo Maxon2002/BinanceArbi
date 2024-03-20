734c1a0fd9b4ebdc5f47a7030cb16bfb1d5ced73
Maxim <haisarov-2002@mail.ru>
2024-03-20 10:55:54 +0500

    aaaa

--- sendPm2Mes.js
+++ sendPm2Mes.js
@@ -3,17 +3,17 @@ const pm2 = require('pm2')
 pm2.connect((err) => {
 
 
-    pm2.sendDataToProcessId({
-        id: 16,
-        type: 'process:msg',
-        data: {
-            morecom: 100
-        },
-        topic: 'comUpdate'
-    }, (err, res) => {
-        if (err) console.error(err);
-        // else console.log(res);
-    });
+    // pm2.sendDataToProcessId({
+    //     id: 16,
+    //     type: 'process:msg',
+    //     data: {
+    //         morecom: 100
+    //     },
+    //     topic: 'comUpdate'
+    // }, (err, res) => {
+    //     if (err) console.error(err);
+    //     // else console.log(res);
+    // });
 
 
 
@@ -31,34 +31,34 @@ pm2.connect((err) => {
 
 
 
-    // pm2.sendDataToProcessId({
-    //     id: 15,
-    //     type: 'process:msg',
-    //     data: {
-    //         startPriceBtc: 45244,
-    //         baseBtcSmall: 3343
-    //     },
-    //     topic: 'startBtc'
-    // }, (err, res) => {
-    //     if (err) console.error(err);
-    //     // else console.log(res);
-    // });
+    pm2.sendDataToProcessId({
+        id: 16,
+        type: 'process:msg',
+        data: {
+            startPriceBtc: 61448,
+            baseBtcSmall: 0.00021429
+        },
+        topic: 'startBtc'
+    }, (err, res) => {
+        if (err) console.error(err);
+        // else console.log(res);
+    });
 
 
 
 
-    // pm2.sendDataToProcessId({
-    //     id: 15,
-    //     type: 'process:msg',
-    //     data: {
-    //         startPriceEth: 35343,
-    //         baseEthSmall: 34343
-    //     },
-    //     topic: 'startEth'
-    // }, (err, res) => {
-    //     if (err) console.error(err);
-    //     // else console.log(res);
-    // });
+    pm2.sendDataToProcessId({
+        id: 16,
+        type: 'process:msg',
+        data: {
+            startPriceEth: 3096.6,
+            baseEthSmall: 0.00378571
+        },
+        topic: 'startEth'
+    }, (err, res) => {
+        if (err) console.error(err);
+        // else console.log(res);
+    });
 
 
 })
