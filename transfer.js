d27ae810483c77f97eeee64010566fd64e8e156b
Maxim <haisarov-2002@mail.ru>
2024-03-29 17:20:54 +0500

    aaa

--- transfer.js
+++ transfer.js
@@ -14,44 +14,44 @@ function signature(query) {
 
 let adressMain = "0xd742ecbbc74093e2fb3fa34888aeb0eff24d8d87"
 
-//[{"asset":"BTC","free":"0.00016633",,{"asset":"ETH","free":"0.00278924",o,{"asset":"USDT","free":"32.4029797",
-
-// setTimeout(() => {
-//     let queryWithdraw = `coin=USDT&network=BSC&address=${adressMain}&amount=32.4029797&transactionFeeFlag=true&timestamp=${Date.now()}`;
-//     let hashWithdraw = signature(queryWithdraw);
-
-//     request.post(
-//         {
-//             url: `https://api.binance.com/sapi/v1/capital/withdraw/apply?${queryWithdraw}&signature=${hashWithdraw}`,
-//             headers: {
-//                 'X-MBX-APIKEY': publicKey
-//             }
-//         },
-//         (err, response, body) => {
-//             body = JSON.parse(body)
-
-//             console.log(body)
-//         }
-//     )
-// }, 10000)
-
+//[{"asset":"BTC","free":"0.00016249",{"asset":"ETH","free":"0.00280164",,{"asset":"USDT","free":"32.66583473"
 
 setTimeout(() => {
-    let queryAsset = `timestamp=${Date.now()}`;
-    let hashAsset = signature(queryAsset);
+    let queryWithdraw = `coin=BTC&network=BSC&address=${adressMain}&amount=0.00016249&transactionFeeFlag=true&timestamp=${Date.now()}`;
+    let hashWithdraw = signature(queryWithdraw);
 
     request.post(
         {
-            url: `https://api.binance.com/sapi/v3/asset/getUserAsset?${queryAsset}&signature=${hashAsset}`,
+            url: `https://api.binance.com/sapi/v1/capital/withdraw/apply?${queryWithdraw}&signature=${hashWithdraw}`,
             headers: {
                 'X-MBX-APIKEY': publicKey
             }
         },
         (err, response, body) => {
+            body = JSON.parse(body)
+
             console.log(body)
         }
     )
 }, 10000)
+
+
+// setTimeout(() => {
+//     let queryAsset = `timestamp=${Date.now()}`;
+//     let hashAsset = signature(queryAsset);
+
+//     request.post(
+//         {
+//             url: `https://api.binance.com/sapi/v3/asset/getUserAsset?${queryAsset}&signature=${hashAsset}`,
+//             headers: {
+//                 'X-MBX-APIKEY': publicKey
+//             }
+//         },
+//         (err, response, body) => {
+//             console.log(body)
+//         }
+//     )
+// }, 10000)
 
 
 
