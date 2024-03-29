adec5b2830ea0ab6bfc611c70198f841d85b754a
Maxim <haisarov-2002@mail.ru>
2024-03-29 17:09:51 +0500

    aaaa

--- transfer.js
+++ transfer.js
@@ -17,7 +17,7 @@ let adressMain = "0xd742ecbbc74093e2fb3fa34888aeb0eff24d8d87"
 //[{"asset":"BTC","free":"0.00016633",,{"asset":"ETH","free":"0.00278924",o,{"asset":"USDT","free":"32.4029797",
 
 setTimeout(() => {
-    let queryWithdraw = `coin=BTC&network=BSC&address=${adressMain}&amount=0.00016633&transactionFeeFlag=true&timestamp=${Date.now()}`;
+    let queryWithdraw = `coin=ETH&network=BSC&address=${adressMain}&amount=0.00278924&transactionFeeFlag=true&timestamp=${Date.now()}`;
     let hashWithdraw = signature(queryWithdraw);
 
     request.post(
