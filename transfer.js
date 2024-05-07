35e7d15e5fb9d64b8febe1e1b3eb24bdf7e500b8
Maxim <haisarov-2002@mail.ru>
2024-05-07 15:26:59 +0500

    aaa

--- transfer.js
+++ transfer.js
@@ -1,5 +1,5 @@
-let secretKey = 'ZfnNUtvL3GNAFz8jrNLC27QEX2s0KeJudytOmlN8N37qnEPd9ZJAWtTsuay4mMRr'
-let publicKey = 'ttNTnSDBy8s5NXgBMdnR3EWwEA8SsgxZzIaucK693hL7V2wGWlJhvrBeSN4HDH5f'
+let secretKey = 'm81gZOGXwXeFzG4JJvWG28YzjNT3q6vcCTy0fsJeUagUSzDnN0JaKUBG3Mozidh2'
+let publicKey = 'ttkhWxHi70KSZN6zaKcVaLDeJuoaAySPptrRiRWiXhtad507pO63SdKaRo7V1Wic'
 
 const request = require('request')
 
@@ -12,46 +12,46 @@ function signature(query) {
         .digest('hex');
 }
 
-let adressMain = "0xd742ecbbc74093e2fb3fa34888aeb0eff24d8d87"
+let adressMain = "0x303b4b7a7dc9038c0510af81031918e304410ada"
 
 //958.12333201
 
-setTimeout(() => {
-    let queryWithdraw = `coin=USDT&network=BSC&address=${adressMain}&amount=958.12333201&transactionFeeFlag=true&timestamp=${Date.now()}`;
-    let hashWithdraw = signature(queryWithdraw);
-
-    request.post(
-        {
-            url: `https://api.binance.com/sapi/v1/capital/withdraw/apply?${queryWithdraw}&signature=${hashWithdraw}`,
-            headers: {
-                'X-MBX-APIKEY': publicKey
-            }
-        },
-        (err, response, body) => {
-            body = JSON.parse(body)
-
-            console.log(body)
-        }
-    )
-}, 1000)
-
-
 // setTimeout(() => {
-//     let queryAsset = `timestamp=${Date.now()}`;
-//     let hashAsset = signature(queryAsset);
+//     let queryWithdraw = `coin=USDT&network=BSC&address=${adressMain}&amount=958.12333201&transactionFeeFlag=true&timestamp=${Date.now()}`;
+//     let hashWithdraw = signature(queryWithdraw);
 
 //     request.post(
 //         {
-//             url: `https://api.binance.com/sapi/v3/asset/getUserAsset?${queryAsset}&signature=${hashAsset}`,
+//             url: `https://api.binance.com/sapi/v1/capital/withdraw/apply?${queryWithdraw}&signature=${hashWithdraw}`,
 //             headers: {
 //                 'X-MBX-APIKEY': publicKey
 //             }
 //         },
 //         (err, response, body) => {
+//             body = JSON.parse(body)
+
 //             console.log(body)
 //         }
 //     )
-// }, 10000)
+// }, 1000)
+
+
+setTimeout(() => {
+    let queryAsset = `timestamp=${Date.now()}`;
+    let hashAsset = signature(queryAsset);
+
+    request.post(
+        {
+            url: `https://api.binance.com/sapi/v3/asset/getUserAsset?${queryAsset}&signature=${hashAsset}`,
+            headers: {
+                'X-MBX-APIKEY': publicKey
+            }
+        },
+        (err, response, body) => {
+            console.log(body)
+        }
+    )
+}, 1000)
 
 
 
