8d549acb67f7ef60a12735a5b18d22485029b508
Maxim <haisarov-2002@mail.ru>
2024-05-07 15:30:54 +0500

    aaa

--- transfer.js
+++ transfer.js
@@ -16,42 +16,42 @@ let adressMain = "0x303b4b7a7dc9038c0510af81031918e304410ada"
 
 //958.12333201
 
-// setTimeout(() => {
-//     let queryWithdraw = `coin=USDT&network=BSC&address=${adressMain}&amount=50.7&transactionFeeFlag=true&timestamp=${Date.now()}`;
-//     let hashWithdraw = signature(queryWithdraw);
-
-//     request.post(
-//         {
-//             url: `https://api.binance.com/sapi/v1/capital/withdraw/apply?${queryWithdraw}&signature=${hashWithdraw}`,
-//             headers: {
-//                 'X-MBX-APIKEY': publicKey
-//             }
-//         },
-//         (err, response, body) => {
-//             body = JSON.parse(body)
-
-//             console.log(body)
-//         }
-//     )
-// }, 1000)
-
-
 setTimeout(() => {
-    let queryAsset = `timestamp=${Date.now()}`;
-    let hashAsset = signature(queryAsset);
+    let queryWithdraw = `coin=USDT&network=BSC&address=${adressMain}&amount=50.7&transactionFeeFlag=true&timestamp=${Date.now()}`;
+    let hashWithdraw = signature(queryWithdraw);
 
     request.post(
         {
-            url: `https://api.binance.com/sapi/v3/asset/getUserAsset?${queryAsset}&signature=${hashAsset}`,
+            url: `https://api.binance.com/sapi/v1/capital/withdraw/apply?${queryWithdraw}&signature=${hashWithdraw}`,
             headers: {
                 'X-MBX-APIKEY': publicKey
             }
         },
         (err, response, body) => {
+            body = JSON.parse(body)
+
             console.log(body)
         }
     )
 }, 1000)
+
+
+// setTimeout(() => {
+//     let queryAsset = `timestamp=${Date.now()}`;
+//     let hashAsset = signature(queryAsset);
+
+//     request.post(
+//         {
+//             url: `https://api.binance.com/sapi/v3/asset/getUserAsset?${queryAsset}&signature=${hashAsset}`,
+//             headers: {
+//                 'X-MBX-APIKEY': publicKey
+//             }
+//         },
+//         (err, response, body) => {
+//             console.log(body)
+//         }
+//     )
+// }, 1000)
 
 
 
